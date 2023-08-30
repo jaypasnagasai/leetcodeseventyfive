c65e0763ade1ce885cda13b1837daed4e953244d
jaypasnagasai <75296693+jaypasnagasai@users.noreply.github.com>
2023-08-30 12:52:47 -0700

    Create maxlevelsum.py

---
+++ maxlevelsum.py
@@ -0,0 +1,26 @@
+#1161. Maximum Level Sum of a Binary Tree
+
+class Solution:
+    def maxLevelSum(self, root):
+        max_sum, ans, level = float('-inf'), 0, 0
+
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            level += 1
+            sum_at_current_level = 0
+            # Iterate over all the nodes in the current level.
+            for _ in range(len(q)):
+                node = q.popleft()
+                sum_at_current_level += node.val
+
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            if max_sum < sum_at_current_level:
+                max_sum, ans = sum_at_current_level, level
+           
+        return ans
