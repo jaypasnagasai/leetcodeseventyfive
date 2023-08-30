f638f7874b9bb5d12ad572f42e84c8a53bf99e13
jaypasnagasai <75296693+jaypasnagasai@users.noreply.github.com>
2023-08-30 12:42:40 -0700

    Create longestzigzag.py

---
+++ longestzigzag.py
@@ -0,0 +1,17 @@
+#1372. Longest ZigZag Path in a Binary Tree
+
+class Solution:
+    def longestZigZag(self, root):
+        self.maxi = 0
+
+        def dfs(node, left, right):
+            self.maxi = max(self.maxi, left, right)
+
+            if node.left:
+                dfs(node.left, right + 1, 0)
+
+            if node.right:
+                dfs(node.right, 0, left + 1)
+
+        dfs(root, 0, 0)
+        return self.maxi
