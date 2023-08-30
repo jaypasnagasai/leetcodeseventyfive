f7deff437ee13a214b276bfeaaf9ed4a06e5038d
jaypasnagasai <75296693+jaypasnagasai@users.noreply.github.com>
2023-08-30 12:46:38 -0700

    Create lowestcommonancestor.py

---
+++ lowestcommonancestor.py
@@ -0,0 +1,28 @@
+#236. Lowest Common Ancestor of a Binary Tree
+
+class Solution:
+  def lowestCommonAncestor(self, root, p, q):
+    q_ = collections.deque([root])
+    parent = {root: None}
+    ancestors = set()  # p's ancestors
+
+    # Iterate until we find both p and q
+    while p not in parent or q not in parent:
+      root = q_.popleft()
+      if root.left:
+        parent[root.left] = root
+        q_.append(root.left)
+      if root.right:
+        parent[root.right] = root
+        q_.append(root.right)
+
+    # Insert all of p ancestors
+    while p:
+      ancestors.add(p)
+      p = parent[p]  # P becomes None in the end
+
+    # Go up from q until meet any of p's ancestors
+    while q not in ancestors:
+      q = parent[q]
+
+    return q
