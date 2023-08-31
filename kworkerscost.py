4e905ec2d2c265bf27004f5e668860d1af5abd5c
jaypasnagasai <75296693+jaypasnagasai@users.noreply.github.com>
2023-08-30 19:38:42 -0700

    Create kworkerscost.py

---
+++ kworkerscost.py
@@ -0,0 +1,26 @@
+#2462. Total Cost to Hire K Workers
+=
+
+class Solution:
+    def totalCost(self, costs, k, candidates):
+        q=costs[:candidates]
+        r=costs[max(candidates,len(costs)-candidates):]
+        heapify(q)
+        heapify(r)
+        res=0
+        i=candidates
+        j=len(costs)-candidates-1
+        for _  in range(k):
+            if not r or q and q[0]<=r[0]:
+                res+=heappop(q)
+                if i<=j:
+                    heappush(q,costs[i])
+                    i+=1
+
+            else:
+                res+=heappop(r)
+                if i<=j:
+                    heappush(r,costs[j])
+                    j-=1
+        
+        return res
