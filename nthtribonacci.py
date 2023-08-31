c43e985c516bd39e3289dc7bc15bdfe19fe81898
jaypasnagasai <75296693+jaypasnagasai@users.noreply.github.com>
2023-08-30 19:55:12 -0700

    Create nthtribonacci.py

---
+++ nthtribonacci.py
@@ -0,0 +1,12 @@
+#1137. N-th Tribonacci Number
+
+class Solution:
+    def tribonacci(self, n):
+        if n==0 or n==1:
+            return n
+        elif n==2:
+            return 1
+        a=[0,1,1]
+        for i in range(3,n+1):
+            a.append(a[i-1]+a[i-2]+a[i-3])
+        return a[n]
